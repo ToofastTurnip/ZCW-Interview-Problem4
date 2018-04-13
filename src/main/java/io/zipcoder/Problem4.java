38fb5155850b74e201a8e0078eefb03d5bebcd32
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-13 16:28:04 -0400

    Nesty solution

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Problem4 {
+
+    public String checkPalindrome(String string) {
+        int count = 1;
+        int oddCount = 0;
+        string = sortString(string);
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i)==string.charAt(i-1)) {
+                count++;
+            } else {
+                if (count%2!=0) {
+                    oddCount++;
+                }
+                count = 1;
+            }
+        }
+        if (oddCount > 1) return "NO";
+        return "YES";
+    }
+
+    public String sortString(String string) {
+        char tempArray[] = string.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,40 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    @Test
+    public void palindromeTest1() {
+        Problem4 problem4 = new Problem4();
+        String expected = "YES";
+        String actual = problem4.checkPalindrome("aaabbbb");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void palindromeTest2() {
+        Problem4 problem4 = new Problem4();
+        String expected = "NO";
+        String actual = problem4.checkPalindrome("cdefghmnopqrstuvw");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void palindromeTest3() {
+        Problem4 problem4 = new Problem4();
+        String expected = "YES";
+        String actual = problem4.checkPalindrome("cdcdcdcdeeeef");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sortStringTest() {
+        Problem4 problem4 = new Problem4();
+        String expected = "aaabbb";
+        String actual = problem4.sortString("ababab");
+        Assert.assertEquals(expected, actual);
+    }
+
 }
